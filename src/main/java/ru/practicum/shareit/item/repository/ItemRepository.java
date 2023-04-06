fe70ccfc9d1893c704a9f60f51966c49388d48c2
Georgy <vgera312@yandex.ru>
2023-04-07 00:53:53 +0400

    Исправление ошибок

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.User;
 
 import java.util.List;
 
